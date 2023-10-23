1f2b6aa17fe29027ef91b0bc759e3b7e33b520f2
Alvaro Graca <alvaro.ps.graca@gmail.com>
2023-10-23 16:40:02 +0100

    ah?

--- taxifare/interface/main_local.py
+++ taxifare/interface/main_local.py
@@ -411,7 +411,7 @@ if __name__ == '__main__':
         import sys
         import traceback
 
-        import ipd
+        import ipdb
         extype, value, tb = sys.exc_info()
         traceback.print_exc()
         ipdb.post_mortem(tb)
